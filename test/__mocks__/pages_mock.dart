b273df0c016f1aca1e60d109b6c99c612798b822
Amogh Lele <amogh@dyte.io>
2022-04-19 20:36:55 +0530

    feat: migrate to null safety
    Signed-off-by: Amogh Lele <amogh@dyte.io>

--- test/__mocks__/pages_mock.dart
+++ test/__mocks__/pages_mock.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'package:libgen/src/page_parser.dart';
 
 Future<String> getSearchPage() => http
-    .get('http://libgen.rs/search.php?req=something')
+    .get(Uri.parse('http://libgen.rs/search.php?req=something'))
     .then((res) => res.body);
 
 String htmlPageWithIds(List<int> ids) => '''
@@ -10,7 +10,7 @@ String htmlPageWithIds(List<int> ids) => '''
     <body>
     <table class="c">
       <tr><td>ID</td></tr>
-      ${ids.map((id) => '<tr><td>${id}</td></tr>')}
+      ${ids.map((id) => '<tr><td>$id</td></tr>')}
     </table>
     </body>
   </html>
